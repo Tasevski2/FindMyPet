73ebe273d7b56a5d2c8b56d8d5b87ea996544e54
Viktor <39170279+Tasevski2@users.noreply.github.com>
2023-07-21 15:34:04 +0200

    Update axios-instance.js

--- src/api/axios-instance.js
+++ src/api/axios-instance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_URL } from '@env';
 
 const instance = axios.create({
-  baseURL: 'http://192.168.100.65:8080/api/', //API_URL,
+  baseURL: API_URL, //API_URL,
   headers: {
     post: {
       'Content-Type': 'application/json',
